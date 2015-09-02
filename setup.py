491417aea68a10c0cf33f4a0b82250ec66fa59d1
alfinkel <alfinkel@us.ibm.com>
2015-09-02 08:35:35 -0500

    Update setup.py to read from requirements.txt

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ Python-Cloudant Client Library
 
 from setuptools import setup, find_packages
 
+requirements_file = open('requirements.txt')
+requirements = requirements_file.read().strip().split('\n')
+
 setup_args ={
     'description': 'Python-Cloudant Client Library',
     'include_package_data': True,
-    'install_requires': ['requests==2.7.0'],
+    'install_requires': requirements,
     'name': 'python-cloudant',
     'version': '0.0.6',
     'packages': find_packages('./src'),
