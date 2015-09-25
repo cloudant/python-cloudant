e5cfe30be69f3709876808b1a9a7d8a271b77182
alfinkel <alfinkel@us.ibm.com>
2015-09-25 13:13:07 -0500

    Change credentials.py based on Pylint run
    - Fix trailing whitespace issues
    - Fix unused argument logic

--- src/cloudant/credentials.py
+++ src/cloudant/credentials.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # Copyright (c) 2015 IBM. All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 """
-_dot_cloudant
+_credentials_
 
-Utils to support using a ~/.cloudant INI style config file
-to let users pass creds in
+Utilities to support using a ~/.cloudant INI style configuration file
+to allow users to pass credentials in
 
 """
 import os
@@ -25,15 +25,20 @@ import ConfigParser
 
 def read_dot_couch(
         filename='~/.couch',
-        section='cloudant',
+        section='couchdb',
         username='user',
         password='password'):
-    config_file = os.path.expanduser(filename)
-    config = ConfigParser.RawConfigParser()
-    config.read(config_file)
-    username_value = config.get("couchdb", username)
-    password_value = config.get("couchdb", password)
-    return username_value, password_value
+    """
+    _read_dot_couch_
+
+    If you have a INI file containing a CouchDB section
+    with a username and password in it, you can read it
+    to get the credentials with this function.
+
+    :returns: tuple (user, password)
+
+    """
+    return _read_dot_file(filename, section, username, password)
 
 
 def read_dot_cloudant(
@@ -44,16 +49,25 @@ def read_dot_cloudant(
     """
     _read_dot_cloudant_
 
-    If you have a ~/.cloudant INI file containing a cloudant
-    section with a username and password in it, you can
-    read it to get the credentials with this function.
+    If you have an INI file containing a Cloudant section
+    with a username and password in it, you can read it
+    to get the credentials with this function.
 
-    :returns: tuple (user, passwd)
+    :returns: tuple (user, password)
 
+    """
+    return _read_dot_file(filename, section, username, password)
+
+def _read_dot_file(filename, section, username, password):
+    """
+    __read_dot_file_
+
+    Handles the parsing of the configuration file for the username
+    and password.
     """
     config_file = os.path.expanduser(filename)
     config = ConfigParser.RawConfigParser()
     config.read(config_file)
-    username_value = config.get("cloudant", username)
-    password_value = config.get("cloudant", password)
+    username_value = config.get(section, username)
+    password_value = config.get(section, password)
     return username_value, password_value
