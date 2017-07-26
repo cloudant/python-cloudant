550b00b6c82b404533320fd14930e443088cb5a2
Sam Smith <samsmith@linux.vnet.ibm.com>
2017-07-26 10:32:29 +0100

    Update year in copyright headers

--- src/cloudant/__init__.py
+++ src/cloudant/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2015 IBM. All rights reserved.
+# Copyright (c) 2015, 2017 IBM. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
