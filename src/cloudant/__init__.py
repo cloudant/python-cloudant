a54349d712d12cd2c8cafdb1937736f5533afada
Sam Smith <samsmith@linux.vnet.ibm.com>
2018-09-20 16:52:52 +0100

    Update version to 2.11.0-SNAPSHOT

--- src/cloudant/__init__.py
+++ src/cloudant/__init__.py
@@ -15,7 +15,7 @@
 """
 Cloudant / CouchDB Python client library API package
 """
-__version__ = '2.10.0'
+__version__ = '2.11.0-SNAPSHOT'
 
 # pylint: disable=wrong-import-position
 import contextlib
