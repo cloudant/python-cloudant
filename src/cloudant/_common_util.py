cloudant/python-cloudant

commit f2c12984c325124d72cbdf5154a33b32538da941

Fix "DeprecationWarning" from "collections" (#451)
DeprecationWarning: Using or importing the ABCs from 'collections'
instead of from 'collections.abc' is deprecated, and in 3.8 it will stop
working.

src/cloudant/_common_util.py
@@ -19,7 +19,7 @@ throughout the library.
 
 import sys
 import platform
-from collections import Sequence
+from collections.abc import Sequence
 import json
 
 from ._2to3 import LONGTYPE, STRTYPE, NONETYPE, UNITYPE, iteritems_

commit 99417c011bd4095058c32e157e45e67bbb8f36fd

Fix broken Python 2 build
f2c1298 accidentally broke the build as collections.abc is only available in Python 3.x.

This commit adds a workaround that enables the import that works both on 2 and 3.

src/cloudant/_common_util.py
@@ -19,11 +19,16 @@ throughout the library.
 
 import sys
 import platform
-from collections.abc import Sequence
+import collections
 import json
 
 from ._2to3 import LONGTYPE, STRTYPE, NONETYPE, UNITYPE, iteritems_
 from .error import CloudantArgumentError, CloudantException, CloudantClientException
+
+try:
+    Sequence = collections.abc.Sequence
+except AttributeError:
+    Sequence = collections.Sequence
 
 # Library Constants
 