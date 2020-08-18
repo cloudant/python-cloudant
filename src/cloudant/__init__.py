55a930e4abf659c086b8cfcc4596a948392b0c5d
Bessenyei Balázs Donát <Balazs.Donat.Bessenyei@ibm.com>
2020-08-18 13:34:39 +0200

    Update version to 2.14.1-SNAPSHOT

--- src/cloudant/__init__.py
+++ src/cloudant/__init__.py
@@ -15,7 +15,7 @@
 """
 Cloudant / CouchDB Python client library API package
 """
-__version__ = '2.14.0'
+__version__ = '2.14.1-SNAPSHOT'
 
 # pylint: disable=wrong-import-position
 import contextlib
