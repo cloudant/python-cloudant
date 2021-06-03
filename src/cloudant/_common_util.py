4b7dc8d6bbe8f3c8e7329509fd48a39b6ed2005b
ildi <ildiko.eigel@ibm.com>
2021-06-03 14:54:41 +0200

    Fix pylint

--- src/cloudant/_common_util.py
+++ src/cloudant/_common_util.py
@@ -209,7 +209,7 @@ def _py_to_couch_translate(key, val, encoder=None):
     try:
         if key in ['keys', 'endkey_docid', 'startkey_docid', 'stale', 'update']:
             return {key: val}
-        if key in ['endkey', 'key', 'startkey'] and type(val) is not (int or LONGTYPE or bool):
+        if key in ['endkey', 'key', 'startkey'] and not isinstance(val, (int, LONGTYPE, bool)):
             return {key: json.dumps(val, cls=encoder)}
         if val is None:
             return {key: None}
